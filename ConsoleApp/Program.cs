da6f70c4a6a53f04a6114ddc4f5c9a384550a38e
bli1962 <beeeeeen.li@outlook.com>
2022-09-07 08:19:00 +1000

    2nd update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Jenkins.");
+
+            Console.WriteLine("Hello World from Jenkins, 2nd update.");
         }
     }
 }
