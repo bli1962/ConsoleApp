000eb3e1c01ae0685e04f1294f43c9d4ac071737
Ben Li <52320080+bli1962@users.noreply.github.com>
2022-09-17 09:31:14 +1000

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,6 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Jenkins.");
-
             Console.WriteLine("Hello World from Jenkins, 2nd update.");
             Console.ReadLine();
         }
